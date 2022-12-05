110e485a42cd218ab5061610fee7ddeb6c39c5bd
turtlelazy <ishraqmahid@gmail.com>
2022-12-05 17:45:43 -0500

    removed old code

--- main.cpp
+++ main.cpp
@@ -19,21 +19,6 @@ int main() {
 
   cout << pretty_string << "\n";
   
-  // while (getline(cin, s)) {
-  //   // reads in line by line each iteration and stores to s
-  //   s = removeLeadingSpaces(s);
-  //   if (s[0] == '}') {
-  //     count = count - 1;
-  //   }
-  //   for (int i = 0; i < count; i++) {
-  //     cout << '\t';
-  //   }
-  //   cout << s << endl;
-  //   count = count + countChar(s, '{');
-  //   if (s[s.length() - 1] == '}' && countChar(s, '{') >= 1) {
-  //     count = count - countChar(s, '}');;
-  //   }
-  // }
   return 0;
 }
 
