47315b9ef60a2a585fc48d9813c599db940b8b9d
Natalie Gallo <92805845+natalie-gallo@users.noreply.github.com>
2022-12-06 11:39:14 -0500

    Revert "made the program more modular"

--- main.cpp
+++ main.cpp
@@ -1,50 +1,26 @@
 #include <cctype>
 #include <iostream>
 #include "funcs.h"
-#include <string>
-#include <sstream>
-
 using namespace std;
-string prettify(string input_string);
 
 // main
 int main() {
-  string input_string = "";
-  string s;
-  while (getline(cin, s)) {
-    input_string += s + "\n";
-  }
-
-  string pretty_string = prettify(input_string);
-
-  cout << pretty_string << "\n";
-  
-  return 0;
-}
-
-string prettify(string input_string){
   string s;
   int count = 0;
-  stringstream iss(input_string);
-  string return_string = "";
-  while (getline(iss, s,'\n'))
-  {
+  while (getline(cin, s)) {
     // reads in line by line each iteration and stores to s
     s = removeLeadingSpaces(s);
-    if (s[0] == '}')
-    {
+    if (s[0] == '}') {
       count = count - 1;
     }
-    for (int i = 0; i < count; i++)
-    {
-      return_string += '\t';
+    for (int i = 0; i < count; i++) {
+      cout << '\t';
     }
-    return_string += s + "\n";
+    cout << s << endl;
     count = count + countChar(s, '{');
-    if (s[s.length() - 1] == '}' && countChar(s, '{') >= 1)
-    {
-      count = count - countChar(s, '}');
+    if (s[s.length() - 1] == '}' && countChar(s, '{') >= 1) {
+      count = count - countChar(s, '}');;
     }
   }
-  return return_string;
+  return 0;
 }
